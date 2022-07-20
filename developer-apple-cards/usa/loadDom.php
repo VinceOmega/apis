3d290caf5ff209ede3f7f9b0324de37b1dcb8fc6
Larry Stanfield <vince.omega@gmail.com>
2022-07-20 00:40:05 -0400

    debugging

--- developer-apple-cards/usa/loadDom.php
+++ developer-apple-cards/usa/loadDom.php
@@ -1,7 +1,11 @@
 <?php
 
-use Sunra\PhpSimple\HtmlDomParser;
-$dom = HtmlDomParser::file_get_html( $url );
+try{
+    use Sunra\PhpSimple\HtmlDomParser;
+    $dom = HtmlDomParser::file_get_html( $url );
+} catch(Exception $e){
+    var_dump($e);
+}
 ?>
 
 <iframe src=<?=$url?>
--- developer-apple-cards/usa/visa.php
+++ developer-apple-cards/usa/visa.php
@@ -1,6 +1,10 @@
 <?php
 
+try{
 $elems = $dom->find('html');
 
 var_dump($elems);
+}catch(Exception $e){
+    var_dump($e);
+}
 
